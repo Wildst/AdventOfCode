859234389e21ad5ac8bd66c6a360d886019532be
Lukas De Wilde <lukas.dewilde@ugent.be>
2020-12-09 19:21:47 +0100

    solution day 9

--- 2020/day_9.py
+++ 2020/day_9.py
@@ -1,10 +1,31 @@
 #! /usr/bin/python
 import sys, getopt
 
-def solve_star1():
-    print(read_file())
-def solve_star2():
-    print(read_file())
+def check_sum(l, n):
+    for i, m in enumerate(l):
+        if n - m in l[i:]:
+            return True
+    return False
+
+def solve_star1(preamble_size):
+    numbers = [int(n) for n in read_file()]
+    for i in range(preamble_size, len(numbers)):
+        if not check_sum(numbers[i-preamble_size:i],numbers[i]):
+            print(numbers[i])
+
+def solve_star2(preamble_size):
+    numbers = [int(n) for n in read_file()]
+    for i in range(preamble_size, len(numbers)):
+        if not check_sum(numbers[i-preamble_size:i],numbers[i]):
+            invalid = numbers[i]
+
+    for i in range(len(numbers)):
+        l = 2
+        while i+l < len(numbers) and sum(numbers[i:i+l]) < invalid:
+            l += 1
+        if sum(numbers[i:i+l]) == invalid:
+            print(min(numbers[i:i+l]) + max(numbers[i:i+l]))
+
 
 
 def read_file():
@@ -16,6 +37,7 @@ if __name__ == "__main__":
     infile = sys.argv[0][2:-2] + "in"
     file_dir = "input_files"
     star = 1
+    preamble_size = 25
     try:
         opts, args = getopt.getopt(sys.argv[1:], "12ti:")
     except getopt.GetoptError:
@@ -31,11 +53,12 @@ if __name__ == "__main__":
             star = 2
         if opt == "-t":
             file_dir = "test_files"
+            preamble_size = 5
 
     if star == 1:
-        solve_star1()
+        solve_star1(preamble_size)
     elif star == 2:
-        solve_star2()
+        solve_star2(preamble_size)
 
 
 
