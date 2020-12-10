1627f2194764e8bfdb9ab1b9f9cf9b23263d8af4
Lukas De Wilde <lukas.dewilde@ugent.be>
2020-12-10 14:33:31 +0100

    solve day 10

--- 2020/day_10.py
+++ 2020/day_10.py
@@ -1,10 +1,43 @@
 #! /usr/bin/python
 import sys, getopt
+from functools import lru_cache
 
 def solve_star1():
-    print(read_file())
+    outlets = sorted([int(i) for i in read_file()])
+    differences = [0,0,1]
+    differences[outlets[0] - 1] += 1
+    for i, outlet in enumerate(outlets[:-1]):
+        differences[outlets[i+1] - outlet - 1 ] += 1
+    print(differences)
+    print(differences[0] * differences[2])
+
+@lru_cache
+def get_option_count(rowlength):
+    if rowlength <= 0:
+        return 0
+    if rowlength == 1:
+        return 1
+    # (1, 2, 3, 4)
+    return sum([get_option_count(rowlength -i) for i in range(1, 4) ])
+
+
 def solve_star2():
-    print(read_file())
+    outlets = sorted([int(i) for i in read_file()])
+    outlets = [0, *outlets, outlets[-1] + 3]
+
+    total = 1
+    rowlength = 1
+
+    for i, outlet in enumerate(outlets[:-1]):
+        if outlets[i+1] - outlet == 3:
+            total *= get_option_count(rowlength)
+            rowlength = 1
+        elif outlets[i+1] - outlet == 1:
+            rowlength += 1
+
+    print(total)
+
+
 
 
 def read_file():
