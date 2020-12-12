2246788fae21115bd283fbe15d0588702e45ec2b
Lukas De Wilde <lukas.dewilde@ugent.be>
2020-12-12 14:35:39 +0100

    solve day 12

--- 2020/day_12.py
+++ 2020/day_12.py
@@ -1,10 +1,42 @@
 #! /usr/bin/python
 import sys, getopt
 
+DIRECTIONS = "NESW"
+DIRECTION_DIFFS = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+
+
 def solve_star1():
-    print(read_file())
+    x, y, direction = 0, 0, 1
+
+    for instruction in read_file():
+        amount = int(instruction[1:])
+        if instruction[0] in "LR":
+            direction += -1*(amount//90) if instruction[0] == "L" else 1*(amount//90)
+            direction %= 4
+        else:
+            dx, dy = DIRECTION_DIFFS[direction] if instruction[0] == "F" else DIRECTION_DIFFS[DIRECTIONS.index(instruction[0])]
+            x += dx * amount
+            y += dy * amount
+    print(abs(x) + abs(y))
+
 def solve_star2():
-    print(read_file())
+    ship_x, ship_y = 0, 0
+    waypoint_x, waypoint_y = 10, -1
+
+    for instruction in read_file():
+        amount = int(instruction[1:])
+        if instruction[0] in "LR":
+            for _ in range((amount if instruction[0] == "L" else 360 - amount)//90):
+                waypoint_x, waypoint_y = waypoint_y, -waypoint_x
+        elif instruction[0] == "F":
+            ship_x += waypoint_x * amount
+            ship_y += waypoint_y * amount
+        else:
+            dx, dy = DIRECTION_DIFFS[DIRECTIONS.index(instruction[0])]
+            waypoint_x += dx * amount
+            waypoint_y += dy * amount
+    print(abs(ship_x) + abs(ship_y))
+
 
 
 def read_file():
