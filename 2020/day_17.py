0e88f75b6317c5f3588b6842ad86a756244491f0
Lukas De Wilde <lukas.dewilde@ugent.be>
2020-12-17 19:34:42 +0100

    solve day 17

--- 2020/day_17.py
+++ 2020/day_17.py
@@ -1,10 +1,129 @@
 #! /usr/bin/python
 import sys, getopt
 
+class Cube():
+    def __init__(self, pos, state):
+        self.x, self.y, self.z = pos
+        self.active = state == "#"
+        self.active_neighbour_count = 0
+
+    def get_pos(self):
+        return (self.x, self.y, self.z)
+
+    def get_neighbours(self):
+        return [ (self.x +i, self.y + j, self.z + k) for k in range(-1, 2) for j in range(-1, 2) for i in range(-1, 2) if not (i==0 and j==0 and k==0) ]
+
+    def __str__(self):
+        return str(self.get_pos()) + " " + ("Active" if self.active else "Inactive")
+
+    def __repr__(self):
+        return str(self)
+
+    def recount(self):
+        self.active_neighbour_count = 0
+
+    def check_activation(self):
+        if self.active:
+            self.active = self.active_neighbour_count in [2, 3]
+        else:
+            self.active = self.active_neighbour_count == 3
+
 def solve_star1():
-    print(read_file())
+    cubes = {}
+    for y, line in enumerate(read_file()):
+        for x, c in enumerate(line):
+            cubes[(x, y, 0)] = Cube((x, y, 0), c)
+
+    for i in range(6):
+        # create neighbours if necesary
+        needed_cubes = set()
+        for cube in cubes.values():
+            if cube.active:
+                needed_cubes |= set(cube.get_neighbours())
+        for position in needed_cubes:
+            if not position in cubes:
+                cubes[position] = Cube(position, False)
+
+        # recount all neighbours
+        for cube in cubes.values():
+            cube.recount()
+        for cube in cubes.values():
+            if cube.active:
+                for position in cube.get_neighbours():
+                    cubes[position].active_neighbour_count += 1
+
+        # check activation
+        for cube in cubes.values():
+            cube.check_activation()
+
+    print(len([cube for cube in cubes.values() if cube.active]))
+
+
+
+class HyperCube():
+    def __init__(self, pos, state):
+        self.x, self.y, self.z, self.w = pos
+        self.active = state == "#"
+        self.active_neighbour_count = 0
+
+    def get_pos(self):
+        return (self.x, self.y, self.z, self.w)
+
+    def get_neighbours(self):
+        return [ (self.x +i, self.y + j, self.z + k, self.w + l)
+                    for l in range(-1, 2)
+                    for k in range(-1, 2)
+                    for j in range(-1, 2)
+                    for i in range(-1, 2)
+                    if not (i==0 and j==0 and k==0 and l == 0) ]
+
+    def __str__(self):
+        return str(self.get_pos()) + " " + ("Active" if self.active else "Inactive")
+
+    def __repr__(self):
+        return str(self)
+
+    def recount(self):
+        self.active_neighbour_count = 0
+
+    def check_activation(self):
+        if self.active:
+            self.active = self.active_neighbour_count in [2, 3]
+        else:
+            self.active = self.active_neighbour_count == 3
+
 def solve_star2():
-    print(read_file())
+    cubes = {}
+    for y, line in enumerate(read_file()):
+        for x, c in enumerate(line):
+            cubes[(x, y, 0, 0)] = HyperCube((x, y, 0, 0), c)
+
+    for i in range(6):
+        # create neighbours if necesary
+        needed_cubes = set()
+        for cube in cubes.values():
+            if cube.active:
+                needed_cubes |= set(cube.get_neighbours())
+        for position in needed_cubes:
+            if not position in cubes:
+                cubes[position] = HyperCube(position, False)
+
+        # recount all neighbours
+        for cube in cubes.values():
+            cube.recount()
+        for cube in cubes.values():
+            if cube.active:
+                for position in cube.get_neighbours():
+                    cubes[position].active_neighbour_count += 1
+
+        # check activation
+        for cube in cubes.values():
+            cube.check_activation()
+
+    print(len([cube for cube in cubes.values() if cube.active]))
+
+
+
 
 
 def read_file():
