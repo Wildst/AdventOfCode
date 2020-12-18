8bcb69d09be2ed5d91e646b6a282d99f0400504e
Lukas De Wilde <lukas.dewilde@ugent.be>
2020-12-18 19:34:38 +0100

    solve day 18

--- 2020/day_18.py
+++ 2020/day_18.py
@@ -1,10 +1,72 @@
 #! /usr/bin/python
 import sys, getopt
 
+ADD = 1
+MULT = 2
+
+
+def solve_expression(expression):
+    sol = 0
+    parentheses_count = 0
+    action = ADD
+
+    sub_expression = ""
+
+    for c in expression:
+        if c == "+":
+            if parentheses_count == 0:
+                if sub_expression.strip():
+                    if action == ADD:
+                        sol += int(sub_expression)
+                    else:
+                        sol *= int(sub_expression)
+                    sub_expression = ""
+                action = ADD
+            else:
+                sub_expression += c
+        elif c == "*":
+            if parentheses_count == 0:
+                if sub_expression.strip():
+                    if action == ADD:
+                        sol += int(sub_expression)
+                    else:
+                        sol *= int(sub_expression)
+                    sub_expression = ""
+                action = MULT
+            else:
+                sub_expression += c
+        elif c == "(":
+            if parentheses_count != 0:
+                sub_expression += c
+            parentheses_count += 1
+        elif c == ")":
+            parentheses_count -= 1
+            if parentheses_count == 0:
+                if action == ADD:
+                    sol += solve_expression(sub_expression)
+                else:
+                    sol *= solve_expression(sub_expression)
+                sub_expression = ""
+            else:
+                sub_expression += c
+        else:
+            sub_expression += c
+
+    if sub_expression.strip():
+        if action == ADD:
+            sol += int(sub_expression)
+        else:
+            sol *= int(sub_expression)
+    return sol
+
+def place_parentheses(expression):
+    return '(' + expression.replace("(", "((").replace(")", "))"). replace("*", ")*(") + ')'
+
 def solve_star1():
-    print(read_file())
+    print(sum(solve_expression(e) for e in read_file()))
 def solve_star2():
-    print(read_file())
+    #print(*[solve_expression(place_parentheses(e)) for e in read_file()])
+    print(sum(solve_expression(place_parentheses(e)) for e in read_file()))
 
 
 def read_file():
