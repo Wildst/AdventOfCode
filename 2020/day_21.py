b80cc1ca7896afe81605dedb886d286c9fd2f323
Lukas De Wilde <lukas.dewilde@ugent.be>
2020-12-21 14:45:27 +0100

    solve day 21

--- 2020/day_21.py
+++ 2020/day_21.py
@@ -1,10 +1,106 @@
 #! /usr/bin/python
 import sys, getopt
+from functools import reduce
+UNKNOWN = "unknown risks"
+
+class Allergen():
+    def __init__(self, name, food_options):
+        self.name = name
+        self.food = set(food_options)
+
+    def found_origin(self):
+        if isinstance(self.food, str):
+            return self.food
+        return False
+
+    def check_origin(self):
+        if not self.found_origin() and len(self.food) == 1:
+            self.food = self.food.pop()
+            return self.food
+
+        return False
+
+    def remove(self, food):
+        if not self.found_origin() and food in self.food:
+            self.food.remove(food)
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def __str__(self):
+        if self.found_origin():
+            return self.name + " found in " + self.food
+        return self.name + " found in " + " or ".join(self.food) + "."
+
+    def __repr__(self):
+        return str(self)
+
+class Food():
+    def __init__(self, name):
+        self.name = name
+        self.amounts_used = 1
+        self.allergen = UNKNOWN
+
+    def __eq__(self, other):
+        return self.name == other.name
+
+    def is_used(self):
+        self.amounts_used += 1
+
 
 def solve_star1():
-    print(read_file())
+    all_foods = {}
+    all_allergens = {}
+    for line in read_file():
+        recipe_foods, allergens = line[:-1].split(" (contains ")
+        recipe_foods = recipe_foods.split(" ")
+        for food in recipe_foods:
+            if food not in all_foods:
+                all_foods[food] = Food(food)
+            else:
+                all_foods[food].is_used()
+        for allergen in allergens.split(", "):
+            if allergen in all_allergens:
+                all_allergens[allergen].food &= set(recipe_foods)
+            else:
+                all_allergens[allergen] = Allergen(allergen, recipe_foods)
+
+    dangerous_ingredients = reduce(lambda a, b: a | b, [a.food for a in all_allergens.values()])
+    print(sum(all_foods[food].amounts_used for food in all_foods if not food in dangerous_ingredients))
+
+
+
+
+
 def solve_star2():
-    print(read_file())
+    all_foods = {}
+    all_allergens = {}
+    for line in read_file():
+        recipe_foods, allergens = line[:-1].split(" (contains ")
+        recipe_foods = recipe_foods.split(" ")
+        for food in recipe_foods:
+            if food not in all_foods:
+                all_foods[food] = Food(food)
+            else:
+                all_foods[food].is_used()
+        for allergen in allergens.split(", "):
+            if allergen in all_allergens:
+                all_allergens[allergen].food &= set(recipe_foods)
+            else:
+                all_allergens[allergen] = Allergen(allergen, recipe_foods)
+
+    dangerous_ingredients = reduce(lambda a, b: a | b, [a.food for a in all_allergens.values()])
+
+    while [allergen for allergen in all_allergens.values() if not allergen.found_origin()]:
+        for allergen in all_allergens.values():
+            source = allergen.check_origin()
+            if source:
+                for other in all_allergens.values():
+                    if not other.found_origin():
+                        other.remove(source)
+
+    print(",".join([allergen.food for allergen in sorted(all_allergens.values(), key=lambda a : a.name)]))
+
 
 
 def read_file():
