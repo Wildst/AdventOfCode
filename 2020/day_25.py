8710e06450b113d8faa68528af0d6878d5ba9b34
Lukas De Wilde <lukas.dewilde@ugent.be>
2020-12-25 19:54:06 +0100

    final part for 2020

--- 2020/day_25.py
+++ 2020/day_25.py
@@ -1,10 +1,35 @@
 #! /usr/bin/python
 import sys, getopt
 
+def transform(value=1, subject_number=7, loop_size=1):
+    for _ in range(loop_size):
+        value *= subject_number
+        value %= 20201227
+
+    return value
+
+
 def solve_star1():
-    print(read_file())
+    card_pk, door_pk = [int(i) for i in read_file()]
+    # calculate card's loop size
+    card = 1
+    card_loop_size = 0
+    while card != card_pk:
+        card = transform(value=card, subject_number=7)
+        card_loop_size += 1
+
+    door = 1
+    door_loop_size = 0
+    while door != door_pk:
+        door = transform(door, subject_number=7)
+        door_loop_size += 1
+
+
+    print(transform(subject_number=door_pk, loop_size=card_loop_size), transform(subject_number=card_pk, loop_size=door_loop_size))
+
 def solve_star2():
-    print(read_file())
+    pass
+    # No second part for the last day :(
 
 
 def read_file():
