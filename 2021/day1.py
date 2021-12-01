e0590b3ef78b8497c4373ab2575041c74f0c9b44
wildst <lukasdewilde0@gmail.com>
2021-12-01 10:42:03 +0100

    solved day1

---
+++ 2021/day1.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python3.8
+import sys, getopt
+
+def solve_star1():
+    counter = 0
+    depths = [int(line) for line in read_file()]
+    for i in range(len(depths)-1):
+        counter += depths[i] < depths[i+1]
+    return counter
+
+
+def solve_star2():
+    counter = 0
+    depths = [int(line) for line in read_file()]
+    for i in range(len(depths)-3):
+        counter += sum(depths[i:i+3]) < sum(depths[i+1:i+4])
+    return counter
+
+
+def read_file():
+    with open(file_dir + "/" + infile) as file:
+        return [line.strip() for line in file]
+
+
+if __name__ == "__main__":
+    infile = sys.argv[0][2:-2] + "in"
+    file_dir = "input_files"
+    star = 1
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "12ti:")
+    except getopt.GetoptError:
+        print("day_<X>.py [12t] [-i <inputfile>]")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt == "-i":
+            infile = arg
+        elif opt == "-1":
+            star = 1
+        elif opt == "-2":
+            star = 2
+        if opt == "-t":
+            file_dir = "test_files"
+
+    if star == 1:
+        print(solve_star1())
+    elif star == 2:
+        print(solve_star2())
+
+
+
+
+
+
+
+
+
