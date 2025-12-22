b5c73d56813cc92556c79e91bf083dc38235895a
Lukas De Wilde <lukas.dewilde@jabbla.com>
2025-12-22 11:22:56 +0100

    solve day 5

--- 2025/day_05.py
+++ 2025/day_05.py
@@ -1,11 +1,65 @@
 #! /usr/bin/python
 import sys, getopt
 
-def solve_star1():
-    return read_file()
-def solve_star2():
-    return read_file()
+class Range:
+    def __init__(self, line):
+        self.start, self.end = map(int,line.split("-"))
 
+    def __contains__(self, ingredient):
+        return self.start <= int(ingredient) <= self.end
+
+    def combine(self, other):
+        combined = False
+        if self.start <= other.start <= self.end:
+            self.end = max(self.end, other.end)
+            combined = True
+        if self.start <= other.end <= self.end:
+            self.start = min(self.start, other.start)
+            combined = True
+        if self.start > other.start and self.end < other.end:
+            self.start = other.start
+            self.end = other.end
+            combined = True
+        return combined
+
+    def count_ingredients(self):
+        return self.end - self.start + 1
+
+
+def solve_star1():
+    find_ranges = True
+    ranges = []
+
+    fresh = []
+    for line in read_file():
+        if not line:
+            find_ranges = False
+            continue
+        if find_ranges:
+            ranges.append(Range(line))
+        else:
+            for range in ranges:
+                if line in range:
+                    fresh.append(line)
+                    break
+
+    return len(fresh)
+
+def solve_star2():
+    ranges = set()
+
+    for line in read_file():
+        if not line:
+            break
+        range = Range(line)
+        remaining = set()
+        for other in ranges:
+            if not range.combine(other):
+                remaining.add(other)
+        ranges = remaining
+        ranges.add(range)
+
+    return sum(range.count_ingredients() for range in ranges)
 
 def read_file():
     with open(file_dir + "/" + infile) as file:
