80b631c579cf3c4afd61191008c0b857879195b7
Lukas De Wilde <lukas.dewilde@jabbla.com>
2025-12-22 11:03:46 +0100

    solve day_04

--- 2025/day_04.py
+++ 2025/day_04.py
@@ -1,10 +1,44 @@
 #! /usr/bin/python
 import sys, getopt
 
+def parse_rolls(lines):
+    rolls = set()
+    for y, line in enumerate(lines):
+        for x, pos in enumerate(line):
+            if pos == "@":
+                rolls.add((x,y))
+    return rolls
+
+def get_neighbour_positions(pos):
+    x, y = pos
+    neighbours = set()
+    for dx in range(-1,2):
+        for dy in range(-1,2):
+            if dx == 0 and dy == 0:
+                continue
+            neighbours.add((x+dx, y+dy))
+    return neighbours
+
+def get_neighbours(pos, grid):
+    return get_neighbour_positions(pos).intersection(grid)
+
+
 def solve_star1():
-    return read_file()
+    rolls = parse_rolls(read_file())
+    accessible = [roll for roll in rolls if len(get_neighbours(roll, rolls)) < 4 ]
+    return len(accessible)
 def solve_star2():
-    return read_file()
+    rolls = parse_rolls(read_file())
+    removed = 0
+    accessible = set(roll for roll in rolls if len(get_neighbours(roll, rolls)) < 4)
+    while accessible:
+        removed += len(accessible)
+        print( len(accessible) )
+        for roll in accessible:
+            rolls.remove(roll)
+        accessible = set(roll for roll in rolls if len(get_neighbours(roll, rolls)) < 4)
+
+    return removed
 
 
 def read_file():
