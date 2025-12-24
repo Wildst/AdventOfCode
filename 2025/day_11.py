90b4f9a5c679b737ad7fc59d3ad3e5fc4087d5cb
Lukas De Wilde <lukas.dewilde@jabbla.com>
2025-12-24 14:16:00 +0100

    partially solve day 11

--- 2025/day_11.py
+++ 2025/day_11.py
@@ -1,10 +1,69 @@
 #! /usr/bin/python
 import sys, getopt
 
+class Node:
+    def __init__(self, line, network):
+        source, *destination = line.split()
+        self.name = source.strip(":")
+        self.connections = [*destination]
+        self.routes_to_you = 0
+        self.network = network
+        network[self.name] = self
+
+        self.paths = set()
+
+    def add_connections_to_you(self, connection_count):
+        self.routes_to_you += connection_count
+        for nodeName in self.connections:
+            if nodeName not in self.network:
+                Node(nodeName, self.network)
+            self.network[nodeName].add_connections_to_you(connection_count)
+
+    def add_path(self, path):
+        if self.name in path:
+            return
+        representation = str(sorted(path))
+        if representation in self.paths:
+            return
+        self.paths.add(representation)
+        longer_path = set(path)
+        longer_path.add(self.name)
+
+        for nodeName in self.connections:
+            if nodeName not in self.network:
+                Node(nodeName, self.network)
+            self.network[nodeName].add_path(longer_path)
+
+    def __repr__(self):
+        return "Node: %s(%s): %s\n" % (self.name, self.routes_to_you, " ".join(self.connections))
+
+    def get_connections(self, nodeName):
+        return [path for path in self.paths if nodeName in path]
+
+
+
 def solve_star1():
-    return read_file()
+    network = {}
+    for line in read_file():
+        Node(line, network)
+
+    if "out" not in network:
+        Node("out", network)
+
+    network["you"].add_connections_to_you(1)
+    return network["out"].routes_to_you
+
 def solve_star2():
-    return read_file()
+    network = {}
+    for line in read_file():
+        Node(line, network)
+
+    if "out" not in network:
+        Node("out", network)
+
+    network["svr"].add_path(set())
+    connections = [connection for connection in network["out"].get_connections("svr") if "fft" in connection and "dac" in connection]
+    return len(connections)
 
 
 def read_file():
